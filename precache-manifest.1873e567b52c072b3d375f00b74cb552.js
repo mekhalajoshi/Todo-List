3f4f6bc900486b9ea961f5bc326395d1cf1a44f9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-14 00:18:11 +0000

    Deploy mekhalajoshi/Todo-List to github.com/mekhalajoshi/Todo-List.git:gh-pages

---
+++ precache-manifest.1873e567b52c072b3d375f00b74cb552.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfa86944f2e80d37d654c9cef186654",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "55458394289ffc799c01",
+    "url": "/Todo-List/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e9f20bfe7bc7f6841c37",
+    "url": "/Todo-List/static/js/2.d7fc8c84.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/Todo-List/static/js/2.d7fc8c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55458394289ffc799c01",
+    "url": "/Todo-List/static/js/main.4eaac13a.chunk.js"
+  },
+  {
+    "revision": "59e346ee62e6b651920e",
+    "url": "/Todo-List/static/js/runtime-main.9604111c.js"
+  }
+]);
